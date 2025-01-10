e1e9ba50f86759dff74f2c7dddee3de9383d0a23
chanhl22 <lch9502@naver.com>
2025-01-10 23:02:42 +0900

    feat: find()

---
+++ src/main/java/hello/mongobasic/read/Main.java
@@ -0,0 +1,33 @@
+package hello.mongobasic.read;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import hello.mongobasic.model.Product;
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+public class Main {
+    public static void main(String[] args) {
+        CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+        CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
+
+        String uri = "mongodb://localhost:27017";
+        MongoClient mongoClient = MongoClients.create(uri);
+        MongoDatabase database = mongoClient.getDatabase("de-mongodb").withCodecRegistry(pojoCodecRegistry);
+        MongoCollection<Product> productCollection = database.getCollection("products", Product.class);
+
+        MongoCursor<Product> cursor = productCollection.find().cursor();
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+        }
+    }
+
+}
